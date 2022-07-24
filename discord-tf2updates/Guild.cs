3faf590c3dddbce0234c41bd027fc77a2c7e3e4e
thomasturner <tomturner279@outlook.com>
2022-07-24 15:44:21 +0100

    Added Json Template

---
+++ discord-tf2updates/Guild.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace discordtf2updates
+{
+    public class Guild
+    {
+        public SocketMessage Message { get; set; }
+        public IMessageChannel Channel { get; set; }
+    }
+}
